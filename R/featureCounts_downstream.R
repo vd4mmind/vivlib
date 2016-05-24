95cec1c250b0c1889dda00b83e1fce61d987388f
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2016-05-24 15:35:39 +0200

    new function for transforming featurecount output to means

--- R/featureCounts_downstream.R
+++ R/featureCounts_downstream.R
@@ -37,3 +37,62 @@ plot_fCountSummary <- function(summaryFile, CutFromHeader, outFile = NULL){
         }
 }
 
+
+#' Transform a featurecount output to a data frame of library-normalized mean counts
+#'
+#' @param fcoutOutput Featurecount output in a data frame
+#' @param samplenames The name of samples (exclusing replicate ID etc). Replicates of the samples
+#'                      will be grouped together for calculating mean..
+#' @param filterByCount A value, if given it will first filter genes by raw mean expression <= given threshold.
+#' @param boxplot Logical. Draw a boxplot or normalized counts?
+#'
+#' @return A data frame of library-normalized mean counts
+#' @export
+#'
+#' @examples
+#'fcount_meantransform(fcoutOutput, samplenames, filterByCount = 1000, boxplot = TRUE)
+#'
+
+fcount_meantransform <- function(fcountOutput, samplenames, filterByCount = NULL, boxplot = TRUE){
+        # make rownames = geneIDs
+        rownames(fcountOutput) <- fcountOutput[,1]
+        fcountOutput <- fcountOutput[7:ncol(fcountOutput)]
+
+        # Filter genes by mean count over all samples
+        if(!is.null(filterByCount)){
+                meanexp <- rowMeans(fcountOutput,na.rm = TRUE)
+                oldnum <- nrow(fcountOutput)
+                fcountOutput <- fcountOutput[meanexp <= filterByCount,]
+                filteredNum <- oldnum - nrow(fcountOutput)
+                print(paste0("Filtered ", filteredNum, " entries!"))
+        }
+
+        # A function to get normalized mean of replicates by sample
+        mean_bysample <- function(name, df){
+                df2 <- dplyr::select(df, contains(name))
+                # get library-norm counts for the df using DESeq2
+                coldata <- data.frame(row.names = colnames(df2), sample = rep(name, ncol(df2)))
+                dds <- DESeq2::DESeqDataSetFromMatrix(df2, colData = coldata,design = ~1)
+                dds <- DESeq2::estimateSizeFactors(dds)
+                df2 <- counts(dds,normalized=TRUE)
+
+                # return rowmeans
+                rmeans <- rowMeans(df2)
+                return(rmeans)
+        }
+
+        output <- sapply(samplenames, mean_bysample, fcountOutput)
+
+        ## make samplewise boxplots if asked
+        if(boxplot){
+                plotdat <- reshape2::melt(output)
+                print(ggplot(plotdat, aes(Var2, value, fill = Var2)) +
+                              geom_boxplot(outlier.color = "red",notch = TRUE) +
+                              labs(x = "Sample", y = "Counts", fill = "Sample", title = "Mean expression by sample")
+                )
+        }
+
+        return(output)
+}
+
+
